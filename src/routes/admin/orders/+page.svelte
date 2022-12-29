0e36e202727b1876bc17e08dff4d6bf96e95c683
bisaek <huggerbertram@gmail.com>
2022-12-29 14:15:00 +0100

    create a orders page

---
+++ src/routes/admin/orders/+page.svelte
@@ -0,0 +1,78 @@
+<script lang="ts">
+	import { db } from '$lib/firebase';
+	import { whenLoggedIn, isAdmin } from '$lib/firebaseFun';
+	import type { User } from 'firebase/auth';
+	import {
+		collection,
+		deleteDoc,
+		doc,
+		getDoc,
+		getDocs,
+		query
+	} from 'firebase/firestore';
+
+	let admin: boolean;
+	let orders = [];
+
+	whenLoggedIn(async (user: User) => {
+		admin = await isAdmin(user.uid);
+
+		const ordersQuery = query(collection(db, 'orders'));
+		const ordersData = await getDocs(ordersQuery);
+		ordersData.forEach(async (order) => {
+			let orderData = order.data();
+			orderData.id = order.id;
+			orderData.exists = true;
+			orderData.totalPrice = 0;
+			for (let [index, item] of orderData.items.entries()) {
+				const itemRef = doc(db, 'items', item.id);
+				const itemData = await getDoc(itemRef);
+
+				orderData.totalPrice += itemData.data()?.price * item.quantity;
+				orderData.items[index] = { ...item, ...itemData.data() };
+			}
+
+			// console.log()
+			orders.push({ ...orderData });
+			orders = orders;
+		});
+		console.log(orders);
+	});
+
+	function removeOrder(order) {
+		const orderRef = doc(db, 'orders', order.id);
+		deleteDoc(orderRef);
+		order.exists = false;
+		// console.log(orders);
+		orders = orders;
+	}
+</script>
+
+<!-- <h1>{admin}</h1> -->
+
+<div class="flex gap-6 flex-col">
+	{#each orders as order}
+		{#if order.exists}
+			<div>
+				<h2 class="text-xl">products</h2>
+				<ul>
+					{#each order.items as item}
+						<li>
+							{item.name} X {item.quantity}
+						</li>
+					{/each}
+				</ul>
+				<p>price: {order.totalPrice}</p>
+				<div>
+					<button
+						on:click={() => {
+							removeOrder(order);
+						}}
+						class="bg-slate-200 p-3 rounded">cancel</button
+					>
+					<button class="bg-slate-200 p-3 rounded">done</button>
+				</div>
+			</div>
+		{/if}
+	{/each}
+</div>
