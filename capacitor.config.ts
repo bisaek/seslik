01b86530b4956af86714e3ebf8bf37a6c5c06db5
bisaek <huggerbertram@gmail.com>
2023-01-19 15:53:38 +0100

    install capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.se.seslik',
+  appName: 'seslik',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
